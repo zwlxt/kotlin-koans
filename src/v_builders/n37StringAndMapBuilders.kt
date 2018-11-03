e6fa96d21306125850584e06f5e7eba36a3027b0
ZENG Weilin <zengweilin@live.com>
2018-11-03 11:56:54 +0800

    Done chapter v

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -28,12 +28,17 @@ fun todoTask37(): Nothing = TODO(
     """
 )
 
+fun buildMap(build: HashMap<Int, String>.() -> Unit): HashMap<Int, String> {
+    val map = HashMap<Int, String>()
+    map.build()
+    return map
+}
+
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -22,7 +22,7 @@ fun todoTask39(): Nothing = TODO(
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -34,7 +34,19 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+            for ((i,p) in products.withIndex()) {
+                tr {
+                    td(color = getCellColor(i, 0)) {
+                        text(p.description)
+                    }
+                    td(color = getCellColor(i, 1)) {
+                        text(p.price)
+                    }
+                    td(color = getCellColor(i, 2)) {
+                        text(p.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
